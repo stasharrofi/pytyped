e7ca298a4dde6c5b11576f3137381c5a625a6eaf
Shahab Tasharrofi <shahab.tasharrofi@gmail.com>
2020-08-26 00:04:13 -0400

    Added marker for being PEP 561 compliant.

--- setup.py
+++ setup.py
@@ -5,14 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytyped",
-    version="0.0.1",
+    version="0.0.2",
     author="Shahab Tasharrofi",
     author_email="shahab.tasharrofi@gmail.com",
     description="Type-Driven Development for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/stasharrofi/pytyped",
-    packages=setuptools.find_packages(exclude=("tests",)),
+    packages=['pytyped', 'pytyped.macros', 'pytyped.json', 'pytyped.metrics'],
+    package_data= {
+        'pytyped': ['py.typed'],
+        'pytyped.macros': ['py.typed'],
+        'pytyped.json': ['py.typed'],
+        'pytyped.metrics': ['py.typed']
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
