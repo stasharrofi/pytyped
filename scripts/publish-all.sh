e2ddc377555912fb4730144614a8475cf0221032
Shahab Tasharrofi <shahab.tasharrofi@gmail.com>
2022-04-17 09:48:30 -0400

    Fixes/improvements in publishing scripts.

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -1,9 +1,24 @@
+#!/bin/bash
+
+check_vars()
+{
+  var_names=("$@")
+  for var_name in "${var_names[@]}"; do
+    [ -z "${!var_name}" ] && echo "$var_name is unset." && var_unset=true
+  done
+  [ -n "$var_unset" ] && exit 1
+  return 0
+}
+
+check_vars PYPI_USERNAME PYPI_PASSWORD
+
 directories="pytyped-macros pytyped-json pytyped-metrics pytyped"
+
 for d in $directories ; do
   echo Working on package $d
   cd $d
   rm -Rf dist/*
-  ../scripts/build
-  ../scripts/publish
+  ../scripts/build.sh
+  ../scripts/publish.sh
   cd ..
 done
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,2 +1,2 @@
 python -m twine check dist/*
-python -m twine upload dist/* --verbose --skip-existing
+python -m twine upload dist/* --verbose --skip-existing -u $PYPI_USERNAME -p $PYPI_PASSWORD
